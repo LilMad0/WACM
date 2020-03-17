3592f3d66202d3fe387a34e24f4a217ccb5af218
LilMad0 <61454567+LilMad0@users.noreply.github.com>
2020-03-17 16:47:40 +0800

    fixed api

--- python/practice.py
+++ python/practice.py
@@ -5,10 +5,11 @@ import datetime
 # Init Variables
 welcomeMessage = "--- Weather Pyvien ---"
 date = datetime.datetime.now()
-date = date.strftime("%y/%-m/%-d")
+date = date.strftime("%Y/%-m/%-d")
 
 # Welcome to the Weather API
 print(welcomeMessage)
+
 
 # User search
 userSearch = input("Where would you like to check the weather?\n> ")
@@ -20,9 +21,9 @@ data = json.loads(response.read())
 woeid = data[0]['woeid']
 
 # Get Weather
-link = 'https://www.metaweather.com/api/???/' + int(woeid) + '/' + date
+link = 'https://www.metaweather.com/api/location/' + str(woeid) + '/' + date
 response = urlopen(link)
-data = json.loads(response.read()
+data = json.loads(response.read())
 
 # Print it to the screen
 print("Type of weather: " + data[0]["weather_state_name"])
